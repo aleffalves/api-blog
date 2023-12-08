97b815e8959d5ece7034b8a792f7f943e40b2007
HOME\aleff <aleff.alves44@gmail.com>
2023-12-08 06:44:21 -0300

    first commit

---
+++ src/main/java/com/github/aleffalves/apiblog/ApiBlogApplication.java
@@ -0,0 +1,13 @@
+package com.github.aleffalves.apiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBlogApplication.class, args);
+	}
+
+}
